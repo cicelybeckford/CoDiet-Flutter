e1a86889ca98799656a8c68bbc258736fd6a9f61
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2019-11-26 21:50:11 -0500

    Log in and Data pages - backend

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase/firebase.dart';
+import 'package:codiet/pages/root_page.dart';
+import 'package:codiet/utils/auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColorDark: Colors.white,
+      ),
+      home: new RootPage(auth: new FirebaseAuth()),
+      routes: <String, WidgetBuilder>{
+        // 'Journey': (context) => 
+        // 'Summaries': (context) =>
+      },
+    );
+  }
+}
---
+++ lib/models/UserInfo.dart
@@ -0,0 +1,61 @@
+class UserInfo {
+  String goal;
+  String activityLevel;
+  String sex;
+  String name;
+  DateTime dob;
+  double height;
+  double initialWeight;
+  double goalWeight;
+  double bmr;
+  int calories;
+  int age;
+
+  void calculateAge() {
+    final currentDate = DateTime.now();
+    age = (currentDate.difference(dob).inDays / 365).floor();
+  }
+
+  void calculateBmr() {
+    calculateAge();
+    if (sex == "Male") {
+        bmr = (10 * initialWeight) + (6.25 * height ) - (5 * age) + 5;
+    } else {
+        bmr = (10 * initialWeight) + (6.25 * height) - (5 * age) - 161;
+    }
+  }
+
+  void calculateCalories() {
+    double temp = 0;
+    calculateBmr();
+    switch(activityLevel) {
+        case "Sedentary":
+            temp = bmr * 1.2;
+            break;
+        case "Lightly active":
+            temp = bmr * 1.375;
+            break;
+        case "Moderately active":
+            temp = bmr * 1.55;
+            break;
+        case "Highly active":
+            temp = bmr * 1.725;
+            break;
+        case "Super athletic":
+            temp = bmr * 1.9;
+            break;
+    }
+
+    switch(goal) {
+        case "Maintain weight":
+            calories = temp.round();
+            break;
+        case "Lose weight":
+            calories = (temp - 500).round();
+            break;
+        case "Gain weight":
+            calories = (temp + 500).round();
+            break;
+    }
+  }
+}
