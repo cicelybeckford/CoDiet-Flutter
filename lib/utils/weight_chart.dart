0bdfb9ad5a4e3accc7d3714748226bd7778f4222
cicelybeckford <cicelybeckford@users.noreply.github.com>
2019-11-27 16:36:42 -0500

    To update master with cicely branch

---
+++ lib/utils/weight_chart.dart
@@ -0,0 +1,42 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:codiet/models/WeightSeries.dart';
+import 'package:flutter/material.dart';
+
+class WeightJourney extends StatelessWidget {
+  final List<WeightSeries> data;
+
+  WeightJourney({@required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    List<charts.Series<WeightSeries, DateTime>> series = [
+      charts.Series(
+        id: "Weight Journey",
+        data: data,
+        domainFn: (WeightSeries series, _) => series.date,
+        measureFn: (WeightSeries series, _) => series.weight,
+        colorFn: (WeightSeries series, _) => series.barColor,
+      )
+    ];
+    return Center(
+      child: Container(
+        height: 500,
+        width: 800,
+        padding: EdgeInsets.all(20),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                Text("Your Weight Journey", style: const TextStyle(fontSize: 20.0, color: Colors.black)),
+                Expanded(
+                  child: charts.TimeSeriesChart(series, animate: true,),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
